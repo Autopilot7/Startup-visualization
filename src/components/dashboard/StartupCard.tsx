d46f3fac72daf89e1f23794b309bc0185d6405f9
bao-tg <tgb0015@gmail.com>
2024-12-07 18:29:34 +0700

    Revert "Add the static page of the startup information to the startup card"
    This reverts commit 1b1481456d29e6a2a83e35ac0d4e1181bc85baa9.

--- src/components/dashboard/StartupCard.tsx
+++ src/components/dashboard/StartupCard.tsx
@@ -1,8 +1,6 @@
 // components/StartupCard.tsx
 import Badge from '@/components/dashboard/Badge';
-import { DivideCircle } from 'lucide-react';
 import Image from 'next/image';
-import Link from 'next/link';
 
 export type StartupCardProps = {
   name: string;
@@ -42,34 +40,33 @@ export default function StartupCard({
   launch_date,
 }: StartupCardProps) {
   return (
-    <Link href={`/startupinfo`}>
-      <div className="flex w-auto gap-4 rounded-2xl border border-slate-400 bg-white p-2 hover:shadow-lg transition-shadow duration-200 cursor-pointer md:flex-row md:items-center md:p-4">
-        <Image
-          src={logo}
-          width={100}
-          height={100}
-          className="max-md:mx-auto h-20 w-20 object-cover rounded-full md:mx-0 md:h-24 md:w-24"
-          alt={`${name} Logo`} />
-        <div className="space-y-2 text-left max-md:space-y-2">
-          <div className="flex md:flex-row">
-            <div className="flex flex-wrap items-center space-x-3">
-              <p className="text-2xl font-bold md:text-3xl">{name}</p>
-              <div className="w-[2px] bg-gray-300 max-md:h-6 md:block md:h-8"></div>
-              <p className="text-md max-md:text-sm font-light text-slate-500">{short_description}</p>
-            </div>
-          </div>
-          <p className="text-sm max-sm:hidden font-normal text-gray-700 md:text-base">
-            {long_description}
-          </p>
-          <div className="flex flex-wrap justify-start gap-2">
-            <Badge type="category" value={field} />
-            <Badge type="phase" value={phase} />
-            <Badge type="launch_date" value={launch_date} />
-            <Badge type="priority" value={priority} />
-            <Badge type="status" value={status} />
+    <div className="flex w-auto gap-4 rounded-2xl border border-slate-400 bg-white p-2 md:flex-row md:items-center md:p-4">
+      <Image
+        src={logo}
+        width={100}
+        height={100}
+        className="max-md:mx-auto h-20 w-20 object-cover rounded-full md:mx-0 md:h-24 md:w-24"
+        alt={`${name} Logo`}
+      />
+      <div className="space-y-2 text-left max-md:space-y-2">
+        <div className="flex md:flex-row">
+          <div className="flex flex-wrap items-center space-x-3">
+            <p className="text-2xl font-bold md:text-3xl">{name}</p>
+            <div className="w-[2px] bg-gray-300 max-md:h-6 md:block md:h-8"></div>
+            <p className="text-md max-md:text-sm font-light text-slate-500">{short_description}</p>
           </div>
         </div>
+        <p className="text-sm max-sm:hidden font-normal text-gray-700 md:text-base">
+          {long_description}
+        </p>
+        <div className="flex flex-wrap justify-start gap-2">
+          <Badge type="category" value={field} />
+          <Badge type="phase" value={phase} />
+          <Badge type="launch_date" value={launch_date} />
+          <Badge type="priority" value={priority} />
+          <Badge type="status" value={status} />
+        </div>
       </div>
-    </Link>
-  )
+    </div>
+  );
 }
